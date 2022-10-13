f8349ffebbb7efca8b5d0938b38bacc6359a06d9
cupicu <ing.jmorando@gmail.com>
2022-10-13 00:14:34 -0500

    css fix2

--- app.js
+++ app.js
@@ -6,16 +6,13 @@ button.onclick = () => {
 }
 
 const loadNewQuote = async () =>{
-
     let res = await fetch('https://api.adviceslip.com/advice');
     let { slip } = await res.json();
-    console.log('req', slip);
 
     let quote = document.getElementById('advice-quote');
     quote.innerText = slip.advice;
-
     let quoteId = document.getElementById('advice-id');
     quoteId.innerText =`ADVICE #${slip.id}`;
-
-
 }
+
+loadNewQuote();
